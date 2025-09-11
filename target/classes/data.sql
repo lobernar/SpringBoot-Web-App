4942471478884abe7d0e5ce7df9ff0e2ce252a1e
Loic Bernard <loic.bernard@ulb.be>
2025-09-11 16:19:36 +0200

    Remove target folder from repository

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
--- Insert only runs if no admin exists.
-INSERT INTO users (username, password, first_name, last_name, email)
-SELECT 'admin', 'password', 'Loic', 'Bernard', 'loic.bernard@test.com'
-WHERE NOT EXISTS (SELECT 1 FROM users WHERE username='admin');
