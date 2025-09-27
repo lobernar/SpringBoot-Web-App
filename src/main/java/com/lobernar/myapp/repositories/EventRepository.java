e2c143509228e056747c37e5e6ce7b0b63ecc224
Loïc Bernard <44466591+lobernar@users.noreply.github.com>
2025-09-27 16:18:41 +0200

    Delete src/main directory

--- src/main/java/com/lobernar/myapp/repositories/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lobernar.myapp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.lobernar.myapp.entities.Event;
-
-public interface EventRepository extends CrudRepository<Event, Integer>{
-}
