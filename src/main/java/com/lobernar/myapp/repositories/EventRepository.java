acad97c0bc7150e5f730ecf4521dd612716cec9d
Loic Bernard <loic.bernard@ulb.be>
2025-10-01 01:31:33 +0200

    Started working on event repository

--- src/main/java/com/lobernar/myapp/repositories/EventRepository.java
+++ src/main/java/com/lobernar/myapp/repositories/EventRepository.java
@@ -1,8 +1,13 @@
 package com.lobernar.myapp.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.lobernar.myapp.entities.Event;
 
 public interface EventRepository extends CrudRepository<Event, Integer>{
+    Optional<Event> findByName(String name);
+    List<Event> findByOwner(String username);
 }
